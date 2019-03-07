e025eb9a03fad9b50b0cba51d3b085112587607a
TomasHendrych <t-hendrych@seznam.cz>
2019-03-07 22:03:31 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,42 @@
+var mista = [
+    {
+        "name":"Hrad Karlštejn",
+        "photo":"../img/misto1.jpg",
+        "link":"https://www.hrad-karlstejn.cz/cs"
+    },
+    {
+        "name":"Hrad Loket",
+        "photo":"../img/misto2.jpg",
+        "link":"https://www.hradloket.cz"
+    },
+    {
+        "name":"Hrad Křivoklát",
+        "photo":"../img/misto3.jpg",
+        "link":"https://www.hrad-krivoklat.cz/cs"
+    },
+    {
+        "name":"Zámek Český Krumlov",
+        "photo":"../img/misto4.jpg",
+        "link":"https://www.zamek-ceskykrumlov.cz/cs"
+    },
+    {
+        "name":"Zámek Telč",
+        "photo":"../img/misto5.jpg",
+        "link":"https://www.zamek-telc.eu/cs"
+    },
+    {
+        "name":"Zámek Konopiště",
+        "photo":"../img/misto6.jpg",
+        "link":"https://www.zamek-konopiste.cz/cs"
+    },
+    {
+        "name":"Zámek Hluboká nad Vltavou",
+        "photo":"../img/misto7.jpg",
+        "link":"https://www.zamek-hluboka.eu/cs"
+    },
+    {
+        "name":"Hrad Houska",
+        "photo":"../img/misto8.jpg",
+        "link":"https://www.hradhouska.cz"
+    },
+];
